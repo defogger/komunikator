b066e84d6dc1f4fc3c29e1a26783b38b3810b2c4
defogger <patryk.bar@student.uj.edu.pl>
2023-01-30 14:09:24 +0100

    Poprawienie build run

--- klient/run.sh
+++ klient/run.sh
@@ -1,2 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+path=$0
+
+len=${#path}
+path=${path:1:len-7}
+
+pwd="${PWD}"
+cd=$pwd
+cd+=$path
+
+echo $cd
+
+cd $cd
+
 java -jar target/klient-1.0-SNAPSHOT-shaded.jar
+
+cd $pwd
--- server/build.sh
+++ server/build.sh
@@ -1,3 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+path=$0
+
+len=${#path}
+path=${path:1:len-9}
+
+pwd="${PWD}"
+cd=$pwd
+cd+=$path
+
+echo $cd
+
+cd $cd
+
 mvn clean
 mvn install
+
+cd $pwd
